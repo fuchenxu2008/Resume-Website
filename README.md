9f58a5f637243f08200bbe8c593c8324585f1cd7
fuchenxu2008 <fuchenxu2008@163.com>
2018-10-30 20:54:17 +0800

    full version with myself as example

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Resume Website Template
 
-A succinct personal website template built with React.
+A succinct responsive personal website template built with React.
+
+## Demo
+![](https://ws4.sinaimg.cn/large/006tNbRwgy1fwqj09gd1bj31kw21q1kx.jpg)
 
 ## Usage
 
-1. Replace your **Resume PDF file** as `src/data/CV.pdf`
-2. Replace your **portrait image** as `src/data/avatar.jpg`
-3. Edit `src/data/info.json` with your personal info.
-4. Run script `yarn serve` or `npm run serve`
+1. Run command `git clone git@github.com:fuchenxu2008/Resume-Website.git` in Terminal and `yarn install` in working directory.
+2. Replace your **Resume PDF file** as `src/data/CV.pdf`
+3. Replace your **portrait image** as `src/data/avatar.jpg`
+4. Edit `src/data/info.json` with your personal info.
+5. Run script `yarn serve` or `npm run serve`
+
+## Notes
+
+* If you wish to leave a section blank in `src/data/info.json`, just leave the outer array `[]` empty, do not add `{}`
--- src/App.js
+++ src/App.js
@@ -1,12 +1,72 @@
 import React, { Component } from 'react';
-import { contact, about, publications } from './data/info.json';
+import { contact, about, publications, research, internship, extracurricular } from './data/info.json';
 import avatar from './data/avatar.jpg';
 import CV from './data/CV.pdf';
+import github from './github.png';
 import './App.css';
 
+const ExpDetail = ({detail}) => (
+  <div className="pub-list">
+    <div className="exp-heading-box">
+      <div className="pub-title">{detail.title}</div>
+      <span>{detail.time}</span>
+    </div>
+    <ul className="detail-list">
+      {detail.efforts.map((effort, i) => (
+        <li key={i}>
+          {effort}
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
 class App extends Component {
+  state = { openNav: false };
+
+  switchNav = () => {
+    this.setState((prevState) => ({
+      openNav: !prevState.openNav
+    }))
+  }
+
   render() {
-    const { name, en_name, address, email, tel } = contact;
+    /**
+     * Navbar
+     */
+    const navbar = (
+      <div className="navbar">
+        {CV && <a href={CV} target="_blank" rel="noopener noreferrer">Resume/CV</a>}
+        { //eslint-disable-next-line
+          <a className="expand-btn iconfont icon-menu" onClick={this.switchNav}></a>
+        }
+        <nav className={this.state.openNav ? "show-nav" : "hide-nav"}>
+          {publications.length > 0 && <a href="#pub">Publications</a>}
+          {research.length > 0 && <a href="#res">Research</a>}
+          {internship.length > 0 && <a href="#intern">Internship</a>}
+          {extracurricular.length > 0 && <a href="#extra">Extracurricular</a>}
+        </nav>
+      </div>
+    )
+    /**
+     * Contact Info
+     */
+    const {name, en_name, email, tel, address, birthday} = contact;
+    const self = (
+      <div className="contact">
+        {avatar && <img src={avatar} alt="avatar" className="avatar-img" />}
+        <ul className="contact-detail">
+          <li><b>{name} ({en_name})</b></li>
+          <li><span>Birthday: </span>{birthday}</li>
+          <li><span>Email: </span>{email}</li>
+          <li><span>Phone: </span>{tel}</li>
+          <li><span>Address: </span>{address}</li>
+        </ul>
+      </div>
+    )
+    /**
+     * Publications
+     */
     const pubs = publications.map((pub, i) => (
       <div className="pub-list" key={i}>
         <div>{pub.publisher}</div>
@@ -14,31 +74,42 @@ class App extends Component {
         <div dangerouslySetInnerHTML={{__html: pub.author}}></div>
       </div>
     ))
+    /**
+     * Researches
+     */
+    const researches = research.map((data, i) => <ExpDetail detail={data} key={i}/>)
+    /**
+     * Internship
+     */
+    const interns = internship.map((data, i) => <ExpDetail detail={data} key={i} />)
+    /**
+     * Extracurricular
+     */
+    const extras = extracurricular.map((data, i) => <ExpDetail detail={data} key={i} />)
+
     return (
       <div>
         <div className="App">
           <h1 className="resume-name" id="top">{name} / {en_name}</h1>
-          <div className="navbar">
-            <a href="#top">Home</a>
-            <a href="#pub">Publications</a>
-            <a href={CV} target="_blank" rel="noopener noreferrer">Resume/CV</a>
-          </div>
-          <div className="contact">
-            <div>
-              <img src={avatar} alt="avatar" className="avatar-img"/>
-            </div>
-            <ul className="contact-detail">
-              <li><b>{name} ({en_name})</b></li>
-              <li><span>Email: </span>{email}</li>
-              <li><span>Phone: </span>{tel}</li>
-              <li><span>Address: </span>{address}</li>
-            </ul>
-          </div>
+          {navbar}
+          {self}
           <p>{about}</p>
-          <h2 className="resume-heading" id="pub">SELECTED PUBLICATIONS</h2>
+          {publications.length > 0 && <h2 className="resume-heading" id="pub">SELECTED PUBLICATIONS</h2>}
           {pubs}
+          {research.length > 0 && <h2 className="resume-heading" id="res">RESEARCH</h2>}
+          {researches}
+          {internship.length > 0 && <h2 className="resume-heading" id="intern">INTERNSHIP</h2>}
+          {interns}
+          {extracurricular.length > 0 && <h2 className="resume-heading" id="extra">EXTRACURRICULAR EXPERIENCES</h2>}
+          {extras}
         </div>
-        <footer>@{en_name}'s Personal HomePage.&nbsp;<a href="#top">Back to Top</a></footer>
+        <footer>
+          <div>
+            @{en_name}'s Personal HomePage.&nbsp;
+            <a href="#top">Back to Top</a>
+          </div>
+          <a href="https://github.com/fuchenxu2008/Resume-Website"><img src={github} alt="" /></a>
+        </footer>
       </div>
     );
   }
