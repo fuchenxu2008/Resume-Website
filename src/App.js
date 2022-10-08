5bda57da12db0732ac60c9eff3d6d2f32fc37c97
fuchenxu2008 <fuchenxu2008@163.com>
2022-10-08 04:19:29 -0700

    new

--- src/App.js
+++ src/App.js
@@ -8,49 +8,34 @@ import {
   skills,
   github,
 } from './data/info.json';
-import avatar from './data/avatar.jpg';
+// import avatar from './data/avatar.jpg';
 import './App.css';
 
-const ExpHeading = ({ title, time }) => (
-  <div className="exp-heading-box">
-    <div className="pub-title">{title}</div>
-    <span>{time}</span>
-  </div>
+const ExpHeading = ({ company, position, time }) => (
+  <>
+    <div className="exp-heading-box">
+      <div className="pub-title">{company}</div>
+      <span>{time}</span>
+    </div>
+    {position && <div className="pub-position">{position}</div>}
+  </>
 );
 
 const BulletList = ({ items }) => (
   <ul className="detail-list">
     {items.map((item, i) => (
-      <li key={i}>{item}</li>
+      <li key={i} dangerouslySetInnerHTML={{ __html: item }} />
     ))}
   </ul>
 );
 
-const Chart = ({ language, proficiency }) => (
-  <div className="chart-item">
-    <div className="chart-proficiency">
-      <div className="chart-bar">
-        <span style={{ width: proficiency }} />
-      </div>
-      <span>
-        {proficiency >= 90
-          ? '非常熟练'
-          : proficiency >= 80
-            ? '较为熟练'
-            : proficiency >= 70
-              ? '熟练'
-              : proficiency >= 50
-                ? '熟悉'
-                : ''}
-      </span>
-    </div>
-    <div>{language}</div>
-  </div>
-);
-
 const ExpDetail = ({ detail }) => (
   <div className="pub-list">
-    <ExpHeading title={detail.title} time={detail.time} />
+    <ExpHeading
+      company={detail.company}
+      position={detail.position}
+      time={detail.time}
+    />
     <div className="pub-detail">
       <BulletList items={detail.efforts} />
       {detail.img && <img src={detail.img} alt="" />}
@@ -65,31 +50,28 @@ const Contact = () => {
   const { name, en_name, email, tel } = contact;
   return (
     <div className="contact">
-      {avatar && <img src={avatar} alt="avatar" className="avatar-img" />}
-      <ul className="contact-detail">
-        <li>
-          <h1>
-            {name} {en_name}
-          </h1>
-        </li>
-        <li>
+      <h1 className="contact-name">
+        {name} {en_name}
+      </h1>
+      <div className="contact-detail">
+        <div>
           <i className="iconfont icon-youxiang" />
-          <span>邮件: &nbsp;</span>
+          {/* <span>邮件: &nbsp;</span> */}
           {email}
-        </li>
-        <li>
+        </div>
+        <div>
           <i className="iconfont icon-phone" />
-          <span>电话: &nbsp;</span>
+          {/* <span>电话: &nbsp;</span> */}
           {tel}
-        </li>
+        </div>
         {github && (
-          <li>
+          <div>
             <i className="iconfont icon-github" />
-            <span>Github: &nbsp;</span>
+            {/* <span>Github: &nbsp;</span> */}
             <a href={github}>{github}</a>
-          </li>
+          </div>
         )}
-      </ul>
+      </div>
     </div>
   );
 };
@@ -101,11 +83,12 @@ const Education = () => (
     <h2 className="resume-heading" id="education">
       教育背景
     </h2>
-    <ExpHeading
-      title={`${education.school} ${education.major}`}
-      time={education.time}
-    />
-    <BulletList items={education.notes} />
+    {education.map((edu) => (
+      <div key={edu.school}>
+        <ExpHeading company={`${edu.school} ${edu.major}`} time={edu.time} />
+        <BulletList items={edu.notes} />
+      </div>
+    ))}
   </Fragment>
 );
 
@@ -159,16 +142,12 @@ const Skills = () => (
     <h2 className="resume-heading" id="skill">
       技能
     </h2>
-    <BulletList items={skills.descriptions} />
-    <div className="chart-list">
-      {skills.charts.map((chart, i) => (
-        <Chart
-          key={i}
-          language={chart.language}
-          proficiency={chart.proficiency}
-        />
-      ))}
-    </div>
+    {Object.entries(skills).map(([key, value], i) => (
+      <div className="skill-item" key={i}>
+        <div className="pub-title">{key}：</div>
+        <div className="skill-detail">{value}</div>
+      </div>
+    ))}
   </Fragment>
 );
 
@@ -187,11 +166,10 @@ class App extends Component {
         <Contact />
         <div className="App">
           <Education />
+          <Skills />
           {workExperiences.length > 0 && <Work />}
-          <br />
           {personalProjects.length > 0 && <Personal />}
           {extracurricular.length > 0 && <Extra />}
-          <Skills />
         </div>
       </div>
     );
